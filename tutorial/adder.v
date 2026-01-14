05ae15f73a2d89cfd5da925faa85d9c6e17f6466
Ian Flint <ian_flint@yahoo.com>
2026-01-13 16:31:36 -0800

    Got the pipelined adder working

--- tutorial/adder.v
+++ tutorial/adder.v
@@ -2,84 +2,102 @@ module pipelined_adder (
         input wire clk,
         input wire a,
         input wire b,
-        input wire carry_in,
-        output reg carry_out,
+        input wire ci,
+        output reg co,
         output reg sum
 );
     always @(posedge clk) begin
-        {carry_out, sum} = a + b + carry_in;
+        {co, sum} <= a + b + ci;
     end
 
 endmodule
 
 module test;
-    parameter MAXINDEX=3;
+    parameter MAXINDEX=8;
     reg clk = 0;
     reg a[MAXINDEX:0];
     reg b[MAXINDEX:0];
     reg ci = 0;
     wire co[MAXINDEX:0];
     wire sum[MAXINDEX:0];
+    genvar ix;
 
-    initial
-    begin
-        //genvar ix;
-        //generate
-        //    for (ix = 0; ix < MAXINDEX + 1; ix += 1) begin
-        //        #0 a[ix] = 0;
-        //        #0 b[ix] = 0;
-        //    end
-        //endgenerate
-        #0 a[0] = 0;
-        #0 a[1] = 0;
-        #0 a[2] = 0;
-        #0 a[3] = 0;
-        #0 b[0] = 0;
-        #0 b[1] = 0;
-        #0 b[2] = 0;
-        #0 b[3] = 0;
-        #0 b[0] = 1;
-        #100 $finish;
+    generate
+        for (ix = 0; ix < MAXINDEX; ix = ix + 1) begin
+            initial begin
+                #0 a[ix] = 0;
+                #0 b[ix] = 0;
+            end
+        end
+    endgenerate
+
+    initial begin
+        #0 a[0] = 1;
+        #400 $finish;
     end
 
     always #1 begin
         clk = !clk;
     end
 
-    reg counter = 0;
-
+    reg[2:0] counter = 0;
     always @(negedge clk) begin
         counter = counter + 1;
-//        if (counter == 0) begin
-            $display ("%t: %x%x%x%x + %x%x%x%x = %x%x%x%x (%x%x%x%x)", 
+        if (counter == 0) begin
+        //if (1) begin
+            $display ("%t: %x%x%x%x%x%x%x%x + %x%x%x%x%x%x%x%x = %x%x%x%x%x%x%x%x (%x%x%x%x%x%x%x%x)", 
                         $time, 
+                        a[7],
+                        a[6],
+                        a[5],
+                        a[4],
                         a[3],
                         a[2],
                         a[1],
                         a[0],
+                        b[7],
+                        b[6],
+                        b[5],
+                        b[4],
                         b[3],
                         b[2],
                         b[1],
                         b[0],
+                        sum[7],
+                        sum[6],
+                        sum[5],
+                        sum[4],
                         sum[3],
                         sum[2],
                         sum[1],
                         sum[0],
+                        co[7],
+                        co[6],
+                        co[5],
+                        co[4],
                         co[3],
                         co[2],
                         co[1],
-                        co[0]
+                        co[0],
                         );
-            b[3] = sum[3];
-            b[2] = sum[2];
-            b[1] = sum[1];
-            b[0] = sum[0];
-            a[0] = a[0] + 1;
-//        end
+            b[7] <= sum[7];
+            b[6] <= sum[6];
+            b[5] <= sum[5];
+            b[4] <= sum[4];
+            b[3] <= sum[3];
+            b[2] <= sum[2];
+            b[1] <= sum[1];
+            b[0] <= sum[0];
+        end
     end
 
-    pipelined_adder a1 (clk, a[0], b[0], ci, co[0], sum[0]);
-    pipelined_adder a2 (clk, a[1], b[1], co[0], co[1], sum[1]);
-    pipelined_adder a3 (clk, a[2], b[2], co[1], co[2], sum[2]);
-    pipelined_adder a4 (clk, a[3], b[3], co[2], co[3], sum[3]);
+    generate
+        for (ix = 0; ix < MAXINDEX; ix = ix + 1) begin :pa_loop
+            if (ix == 0) begin
+                pipelined_adder pa (clk, a[ix], b[ix], ci, co[ix], sum[ix]);
+            end else begin
+                pipelined_adder pa (clk, a[ix], b[ix], co[ix - 1], co[ix], sum[ix]);
+            end
+        end
+    endgenerate
 endmodule
